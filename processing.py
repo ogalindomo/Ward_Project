a362dae9d45cfd89c955d25a4a548b7812bc376c
Oscar Galindo <oscargalindo@Emma.local>
2020-10-31 17:15:46 -0600

    Little spacing changes.

--- processing.py
+++ processing.py
@@ -1,4 +1,5 @@
 import random
+
 def read_sentence():
     s = "I am Oscar Galindo a graduate student in the Master’s of Computer Science program. My ID is 80585887. I am sending this email to ask you to remove the “Pre-requisite and test score error” requirement to join to the class Topics in Data Science, the CRN of the class is 29030."
     return s
