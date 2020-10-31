655d3326da7c02bbf3dfc34bf185827e0cd696bb
Oscar Galindo <oscargalindo@Emma.local>
2020-10-31 16:10:20 -0600

    Added vectorization of sentences.

--- feature_extraction.py
+++ feature_extraction.py
@@ -3,11 +3,10 @@ import processing,re
 def get_words():
     sentence = processing.read_sentence()
     sentences = sentence.split(".")
-    sentence_words = []*len(sentences)
-    for i in range(len(sentences)):
-      sentence_words[i].append(re.findall(r'[a-zA-Z]+[-][a-zA-z]+|[a-zA-Z]+[\’]+[a-zA-Z]+|[a-zA-Z]+|[0-9]+',sentences[i]))
+    sentence_words = []
+    for i in range(len(sentences)-1):
+        sentence_words.append(re.findall(r'[a-zA-Z]+[-][a-zA-z]+|[a-zA-Z]+[\’]+[a-zA-Z]+|[a-zA-Z]+|[0-9]+',sentences[i]))
     return sentence_words
-    
 
 def get_words_expression(sentence):
     return re.findall(r'[a-zA-Z]+[-][a-zA-z]+|[a-zA-Z]+[\’]+[a-zA-Z]+|[a-zA-Z]+|[0-9]+',sentence)
