ad5d9f877a7beab6368ff54fb44e6465f3255aa6
Isaac Bautista <i.bautista96@gmail.com>
2020-11-16 23:21:02 -0700

    scoring program

--- emails.py
+++ emails.py
@@ -1,4 +1,4 @@
-import nltk
+import nltk, csv
 from glob import glob
 import pandas as pd
 import numpy as np
@@ -7,7 +7,7 @@ sentence_tokenizer = nltk.data.load("tokenizers/punkt/english.pickle")
 stopwords = nltk.corpus.stopwords.words("english")
 
 def get_email_files():
-    return glob("./Emails/*/*")
+    return glob("./Emails/*/*.")
 
 def get_random_email():
     email_list = get_email_files()
@@ -45,7 +45,7 @@ def create_sentences(emails):
         # TODO: 
         # clean up the sentences with some regex
         sentences = sentence_tokenizer.tokenize(email)
-        sentences = list(map(lambda x: x.replace("\n", " "), sentences))
+        # sentences = list(map(lambda x: x.replace("\n", " "), sentences))
         
         # print(sentences)
 
@@ -87,7 +87,7 @@ def score_sentence(sentence, word_frequencies):
 
 def summarize_email(email, word_frequencies):
     sentences = sentence_tokenizer.tokenize(email)
-    sentences = list(map(lambda x: x.replace("\n", " "), sentences))
+    # sentences = list(map(lambda x: x.replace("\n", " "), sentences))
 
     scores = list(map(lambda x: score_sentence(x, word_frequencies), sentences))
     print("summarizing email...")
@@ -98,30 +98,87 @@ def summarize_email(email, word_frequencies):
     print(f"max score: {max_score}")
     print(f"summary: \n{sentences[max_score]}\n")
 
+def tokenize_emails():
+    # emails = get_email_files()
+    folders = glob("./Emails/*")
+
+    for f in folders:
+        print(f)
+
+    folder = input("pick a folder: ")
+
+    files = glob("./Emails/"+folder+"/*")
+
+    for f in files:
+        print(f)
+
+    email = input("pick a file: ")
+
+    emails = ["./Emails/" + folder + "/" + email]
+
+    # print(emails)
+    # exit(1)
+
+    for email in emails:
+        print(f"tokenizing: {email}\n")
+        with open(email, "r", encoding="utf-8") as f:
+            with open(email+"scores", "w", encoding="utf-8") as g:
+                data = f.read()
+
+                writer = csv.writer(g)
+
+                # print(data)
+                sentences = sentence_tokenizer.tokenize(data)
+
+                for s in sentences:
+                    scores = [s.replace("\n", "")]
+                    for word in nltk.word_tokenize(s):
+                        # print(word)
+
+                        word_score = input(f"\n{word}: ")
+                        if word_score == "":
+                            word_score = 0.0
+                        scores.append(word_score)
+
+                    sentence_score = input(f"\n0 or 1\n{s}: ")
+                    scores.append(sentence_score)
+
+                    print(scores)
+
+                    writer.writerow(scores)
+
+                # print(sentences)
+                # with open(email+"tokens", "w", encoding="utf-8") as g:
+                #     writer = csv.writer(g, delimiter="\n")
+
+                #     writer.writerows([sentences])
+
+
 
 def main():
-    emails = get_all_emails(limit=None)
-    sentences = create_sentences(emails)
-    # print(sentences)
+    # emails = get_all_emails(limit=25)
+    # sentences = create_sentences(emails)
+    # # print(sentences)
 
-    word_frequencies = create_word_frequencies(sentences)
-    # print(word_frequencies)
+    # word_frequencies = create_word_frequencies(sentences)
+    # # print(word_frequencies)
 
-    # s = sentences[0]
-    # print(f"example sentence: {s}")
-    # print(f"sentence score:   {score_sentence(s, word_frequencies)}")
+    # # s = sentences[0]
+    # # print(f"example sentence: {s}")
+    # # print(f"sentence score:   {score_sentence(s, word_frequencies)}")
 
-    summarize_email(emails[1], word_frequencies)
+    # summarize_email(emails[1], word_frequencies)
 
-    scores = []
-    for sentence in sentences:
-        scores.append(score_sentence(sentence, word_frequencies))
+    # scores = []
+    # for sentence in sentences:
+    #     scores.append(score_sentence(sentence, word_frequencies))
     
-    df = pd.DataFrame({"sentence": sentences, "score": scores})
-    # print(df)
+    # df = pd.DataFrame({"sentence": sentences, "score": scores})
+    # # print(df)
 
-    df.to_csv("scores.csv", index=False)
+    # df.to_csv("scores.csv", index=False)
 
+    tokenize_emails()
 
 if __name__ == "__main__":
     main()
